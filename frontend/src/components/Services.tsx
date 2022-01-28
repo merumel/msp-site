0796aaf980aece3c7dc8c5eea2304564be8f2d2e
merumel <flurfle@gmail.com>
2022-01-27 19:03:32 -0600

    todo: fix height of cards

--- frontend/src/components/Services.tsx
+++ frontend/src/components/Services.tsx
@@ -4,7 +4,7 @@ import { StaticImage } from "gatsby-plugin-image"
 const Services = () => {
 
     return(
-        <div className="flex flex-col bg-[#e4e4e4] text-[#565656]">
+        <div className="flex flex-col bg-slate-700 text-[#e4e4e4]">
             <h1 className="text-center text-2xl pt-20 text-indigo-700 font-bold">Our Services</h1>
             <strong className="text-center font-bold text-4xl">What We Offer</strong>
             <p className="text-base pt-6 text-center text-body-color">
@@ -15,7 +15,7 @@ const Services = () => {
                 <div className="p-20 mt-15 font-sans grid grid-cols-3 grid-flow-row gap-x-4 gap-y-14 auto-rows-auto font-hairline self-start">
                     
                 <div className="flex justify-center items-center">
-                        <div className="rounded shadow-lg bg-white max-w-md h-96">
+                        <div className="rounded shadow-lg bg-slate-500 max-w-md h-96 shadow-indigo-700/50 hover:shadow-xl hover:shadow-amber-700/50">
                         <StaticImage as="div" className="flex h-1/3 w-1/4 m-7" objectFit="contain" src="../images/code.png" alt="Sunset in the mountains"/>
                             <div className="px-6 py-4">
                                 <div className="font-bold text-xl mb-2 leading-6">The Coldest Sunset</div>
@@ -28,7 +28,7 @@ const Services = () => {
                              
                        
                     <div className="flex justify-center items-center">
-                        <div className="rounded shadow-lg bg-white max-w-md h-96">
+                        <div className="rounded shadow-lg bg-slate-500 max-w-md h-96 shadow-indigo-700/50 hover:shadow-xl hover:shadow-amber-700/50">
                         <StaticImage as="div" className="flex h-1/3 w-1/4 m-7" objectFit="contain" src="../images/laptop_1.png" alt="Sunset in the mountains"/>
                             <div className="px-6 py-4">
                                 <div className="font-bold text-xl mb-2 leading-6">The Coldest Sunset</div>
@@ -37,9 +37,9 @@ const Services = () => {
                                 </p>
                             </div>                   
                         </div> 
-                    </div>  
+                    </div>
                     <div className="flex justify-center items-center">
-                        <div className="rounded shadow-lg bg-white max-w-md h-96">
+                        <div className="rounded shadow-lg bg-slate-500 max-w-md h-96 shadow-indigo-700/50 hover:shadow-xl hover:shadow-amber-700/50">
                         <StaticImage as="div" className="flex h-1/3 w-1/4 m-7" objectFit="contain" src="../images/document.png" alt="Sunset in the mountains"/>
                             <div className="px-6 py-4">
                                 <div className="font-bold text-xl mb-2 leading-6">The Coldest Sunset</div>
@@ -50,7 +50,7 @@ const Services = () => {
                         </div> 
                     </div>   
                     <div className="flex justify-center items-center">
-                        <div className="rounded shadow-lg bg-white max-w-md h-96">
+                        <div className="rounded shadow-lg bg-slate-500 max-w-md h-96 shadow-indigo-700/50 hover:shadow-xl hover:shadow-amber-700/50">
                         <StaticImage as="div" className="flex h-1/3 w-1/4 m-7" objectFit="contain" src="../images/cloud.png" alt="Sunset in the mountains"/>
                             <div className="px-6 py-4">
                                 <div className="font-bold text-xl mb-2 leading-6">The Coldest Sunset</div>
@@ -59,9 +59,9 @@ const Services = () => {
                                 </p>
                             </div>                   
                         </div> 
-                    </div>    
+                    </div>  
                     <div className="flex justify-center items-center">
-                        <div className="flex-col rounded shadow-lg bg-white max-w-md h-96">
+                        <div className="rounded shadow-lg bg-slate-500 max-w-md h-96 shadow-indigo-700/50 hover:shadow-xl hover:shadow-amber-700/50">
                         <StaticImage as="div" className="flex h-1/3 w-1/4 m-7" objectFit="contain" src="../images/lock.png" alt="Sunset in the mountains"/>
                             <div className="px-6 py-4">
                                 <div className="font-bold text-xl mb-2 leading-6">The Coldest Sunset</div>
@@ -70,9 +70,9 @@ const Services = () => {
                                 </p>
                             </div>                   
                         </div> 
-                    </div>                
+                    </div>           
                     <div className="flex justify-center items-center">
-                        <div className="rounded shadow-lg bg-white max-w-md h-96">
+                        <div className="rounded shadow-lg bg-slate-500 max-w-md h-96 shadow-indigo-700/50 hover:shadow-xl hover:shadow-amber-700/50">
                         <StaticImage as="div" className="flex h-1/3 w-1/4 m-7" objectFit="contain" src="../images/mail.png" alt="Sunset in the mountains"/>
                             <div className="px-6 py-4">
                                 <div className="font-bold text-xl mb-2 leading-6">The Coldest Sunset</div>
