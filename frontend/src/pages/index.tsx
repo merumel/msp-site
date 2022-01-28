b4c5c1bfd543d8688bd1c4416f50b58b06451c35
merumel <flurfle@gmail.com>
2022-01-27 19:42:59 -0600

    touchups

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -19,7 +19,7 @@ const IndexPage = () => {
             <div
             className="grid grid-cols-1 divide-y-0 items-center"
             > 
-                <div className="flex relative shadow-lg shadow-indigo-400/50 mt-15 font-sans p-10 font-hairline self-start ">
+                <div className="flex relative shadow-lg shadow-indigo-400/50 mt-15 font-sans p-10 font-hairline self-start bg-stone-200">
                     
                     <div className="container justify-center w-1/2 h-1/2 p-20 ml-10">
                         <h1 className="text-3xl md:text-5xl text-black leading-tight">
