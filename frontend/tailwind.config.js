1dd4c0406a9bd540326ec5ccff1d512b205106c9
merumel <flurfle@gmail.com>
2022-02-17 13:40:16 -0600

    mobile changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'custom-gray': '#b0a8a5',
-        'custom-green': '#c4cc74',
-        'custom-pale': '#ded5d1'
+        "custom-gray": "#b0a8a5",
+        "custom-green": "#c4cc74",
+        "custom-pale": "#ded5d1",
       },
-      fontFamily:{
-        sans: ['"Chakra Petch"', 'sans'],
+      fontFamily: {
+        sans: ['"Chakra Petch"', "sans"],
       },
       backgroundImage: {
-        'shiba-laptop': "url('../images/shiblaptop.png')",
-        'city-image': "url('../images/houston.jpg')",
-        'shiba-business': "url('../images/officeshib.jpg')"
-      }
+        "shiba-laptop": "url('../images/shiblaptop.png')",
+        "city-image": "url('../images/houston.jpg')",
+        "shiba-business": "url('../images/officeshib.jpg')",
+      },
+      boxShadow: {
+        "3xl": "3px 3px 0px 3px rgb(100, 100, 100)",
+      },
     },
   },
   plugins: [],
-}
+};
