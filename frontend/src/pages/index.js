57fcc1427d5de16c70b730bddee47069a9af5757
clai <flurfle@gmail.com>
2022-04-23 16:07:25 -0500

    google analytics

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -220,6 +220,15 @@ const IndexPage = () => {
         <div className="shadow-lg shadow-indigo-700/50" id="contact">
           <Contact />
         </div>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-226941930-1">
+        </script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-226941930-1');
+        </script>
       </div>
     </Layout>
   );
